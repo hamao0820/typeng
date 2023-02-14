5979a802644e468d549ec0098239d2d9317aa955
浜田　俊祐 <hamashun0820@icloud.com>
2023-02-14 23:56:42 +0900

    stageWordsListをモーダルで表示

--- components/SelectList.tsx
+++ components/SelectList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
@@ -13,41 +13,40 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 import UnfoldLess from '@mui/icons-material/UnfoldLess';
 import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
 import path from 'path';
-import type { Mode, Rank } from '../types';
+import { Id, Mode, Rank, Stage } from '../types';
 import { sliceByNumber } from '../utils';
 import RankWordsList from './RankWordsList';
+import IdWordsList from './IdWordsList';
+import StageWordsList from './StageWordsList';
 
 type Props = {
-    rank: number;
+    rank: Rank;
     wordsNum: number;
     mode: Mode;
 };
 
 type OpenStatesType = {
-    block: number;
+    id: Id;
     open: boolean;
 };
 
-// TODO: 単語一覧の表示
-// https://mui.com/material-ui/react-dialog/#scrolling-long-content
 const SelectList: React.FC<Props> = ({ rank, wordsNum, mode }) => {
     const [openStates, setOpenStates] = React.useState<OpenStatesType[]>(
         sliceByNumber(
             [...Array(wordsNum)].map((_, i) => i + 1),
             100
-        ).map<OpenStatesType>((_, block) => {
-            return { block, open: false };
+        ).map<OpenStatesType>((_, id) => {
+            return { id: String(id) as Id, open: false };
         })
     );
     const [isRankWordsListModalOpen, setIsRankWordsListModalOpen] = useState<boolean>(false);
-    // const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(false);
-    const closeRankWordListModal = () => {
-        setIsRankWordsListModalOpen(false);
-    };
-    const handleClick = (block: number) => {
+    const [isIdWordsListModalOpen, setIsIdWordsListModalOpen] = useState<boolean>(false);
+    const [isStageWordsListModalOpen, setIsStageWordsListModalOpen] = useState<boolean>(false);
+
+    const handleClick = (id: number) => {
         setOpenStates((prev) =>
             prev.map((state, i) => {
-                if (i !== block) {
+                if (i !== id) {
                     return state;
                 } else {
                     return { ...state, open: !state.open };
@@ -68,13 +67,44 @@ const SelectList: React.FC<Props> = ({ rank, wordsNum, mode }) => {
         });
     };
 
+    const [activeId, setActiveId] = useState<Id | ''>('');
+    useEffect(() => {
+        if (activeId === '') return;
+        setIsIdWordsListModalOpen(true);
+    }, [activeId]);
+
+    const [activeIdAndStage, setActiveIdAndStage] = useState<{ id: Id | ''; stage: Stage | '' }>({ id: '', stage: '' });
+    useEffect(() => {
+        if (activeIdAndStage.id === '' || activeIdAndStage.stage === '') return;
+        setIsStageWordsListModalOpen(true);
+    }, [activeIdAndStage]);
+
     return (
         <div className="m-2 w-screen">
             <RankWordsList
                 rank={String(rank) as Rank}
                 isOpen={isRankWordsListModalOpen}
-                close={closeRankWordListModal}
-            ></RankWordsList>
+                close={() => {
+                    setIsRankWordsListModalOpen(false);
+                }}
+            />
+            <IdWordsList
+                rank={rank}
+                id={activeId === '' ? '0' : activeId}
+                isOpen={isIdWordsListModalOpen}
+                close={() => {
+                    setIsIdWordsListModalOpen(false);
+                }}
+            />
+            <StageWordsList
+                rank={rank}
+                id={activeIdAndStage.id === '' ? '0' : activeIdAndStage.id}
+                stage={activeIdAndStage.stage === '' ? 'all' : activeIdAndStage.stage}
+                isOpen={isStageWordsListModalOpen}
+                close={() => {
+                    setIsStageWordsListModalOpen(false);
+                }}
+            />
             <List
                 sx={{
                     width: '100%',
@@ -119,31 +149,35 @@ const SelectList: React.FC<Props> = ({ rank, wordsNum, mode }) => {
                 }
             >
                 <Divider />
-                {wordIndicesArr.map((wordIndices, block) => {
+                {wordIndicesArr.map((wordIndices, id) => {
                     return (
-                        <React.Fragment key={block}>
+                        <React.Fragment key={id}>
                             <ListItemButton
                                 onClick={() => {
-                                    handleClick(block);
+                                    handleClick(id);
                                 }}
                                 sx={
-                                    openStates[block].open
+                                    openStates[id].open
                                         ? {
                                               bgcolor: 'rgba(0, 0, 0, 0.2)',
                                           }
                                         : {}
                                 }
+                                onContextMenu={(e) => {
+                                    e.preventDefault();
+                                    setActiveId(String(id) as Id);
+                                }}
                             >
                                 <ListItemIcon>
-                                    <span className="text-3xl font-bold">{block + 1}</span>
+                                    <span className="text-3xl font-bold">{id + 1}</span>
                                 </ListItemIcon>
                                 <ListItemText
                                     primary={`${wordIndices[0]}~${wordIndices.slice(-1)[0]}`}
                                     primaryTypographyProps={{ fontSize: '1.3rem', fontWeight: 500, margin: '3px' }}
                                 />
-                                {openStates[block].open ? <ExpandLess /> : <ExpandMore />}
+                                {openStates[id].open ? <ExpandLess /> : <ExpandMore />}
                             </ListItemButton>
-                            <Collapse in={openStates[block].open} timeout="auto" unmountOnExit>
+                            <Collapse in={openStates[id].open} timeout="auto" unmountOnExit>
                                 <List
                                     component="div"
                                     disablePadding
@@ -155,11 +189,20 @@ const SelectList: React.FC<Props> = ({ rank, wordsNum, mode }) => {
                                 >
                                     <Link
                                         href={{
-                                            pathname: path.join(mode, String(rank), String(block)),
+                                            pathname: path.join(mode, String(rank), String(id)),
                                             query: { stage: 'all' },
                                         }}
                                     >
-                                        <ListItemButton sx={{ pl: 4 }}>
+                                        <ListItemButton
+                                            sx={{ pl: 4 }}
+                                            onContextMenu={(e) => {
+                                                e.preventDefault();
+                                                setActiveIdAndStage({
+                                                    id: String(id) as Id,
+                                                    stage: 'all',
+                                                });
+                                            }}
+                                        >
                                             <ListItemIcon>
                                                 <span className="text-xl font-bold">{1}</span>
                                             </ListItemIcon>
@@ -176,14 +219,23 @@ const SelectList: React.FC<Props> = ({ rank, wordsNum, mode }) => {
                                     </Link>
                                     {sliceByNumber(wordIndices, 10).map((wordIndices, i) => {
                                         return (
-                                            <React.Fragment key={`${block}-${i}`}>
+                                            <React.Fragment key={`${id}-${i}`}>
                                                 <Link
                                                     href={{
-                                                        pathname: path.join(mode, String(rank), String(block)),
+                                                        pathname: path.join(mode, String(rank), String(id)),
                                                         query: { stage: String(i) },
                                                     }}
                                                 >
-                                                    <ListItemButton sx={{ pl: 4 }}>
+                                                    <ListItemButton
+                                                        sx={{ pl: 4 }}
+                                                        onContextMenu={(e) => {
+                                                            e.preventDefault();
+                                                            setActiveIdAndStage({
+                                                                id: String(id) as Id,
+                                                                stage: String(i) as Stage,
+                                                            });
+                                                        }}
+                                                    >
                                                         <ListItemIcon>
                                                             <span className="text-xl font-bold">{i + 2}</span>
                                                         </ListItemIcon>
---
+++ components/StageWordsList.tsx
@@ -0,0 +1,46 @@
+import React, { FC, Fragment, useEffect, useState } from 'react';
+import { Id, Rank, Stage, Word } from '../types';
+import Modal from './Modal';
+
+type Props = {
+    rank: Rank;
+    id: Id;
+    stage: Stage;
+    isOpen: boolean;
+    close: () => void;
+};
+
+const StageWordsList: FC<Props> = ({ rank, id, stage, isOpen, close }) => {
+    const [words, setWords] = useState<Word[]>([]);
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`api/${rank}/${id}/${stage}`);
+            const words = (await res.json()) as Word[];
+            setWords(words);
+        })();
+    }, [rank, id, stage]);
+    return (
+        <div>
+            <Modal isOpen={isOpen} close={close}>
+                <div className="h-full overflow-y-scroll">
+                    {words.map((word, i) => {
+                        return (
+                            <Fragment key={i}>
+                                <div className={`flex mx-2 py-2 items-center ${i % 2 === 0 && 'bg-gray-200'}`}>
+                                    <div className="mr-4 ml-1">No.{Number(word.id) + 1}</div>
+                                    <div className="text-lg">{word.en}</div>
+                                    <span className="ml-1 mr-2">:</span>
+                                    <div className="text-lg whitespace-nowrap text-ellipsis overflow-x-hidden">
+                                        {word.ja}
+                                    </div>
+                                </div>
+                            </Fragment>
+                        );
+                    })}
+                </div>
+            </Modal>
+        </div>
+    );
+};
+
+export default StageWordsList;
