ab35cbc6b1de3c95e3997bde8a8ab08722e6bf54
浜田　俊祐 <hamashun0820@icloud.com>
2023-02-14 19:31:30 +0900

    Modalコンポーネントを作成

---
+++ components/Modal.tsx
@@ -0,0 +1,28 @@
+import React, { Dispatch, FC, Fragment, ReactNode, SetStateAction } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+
+type Props = {
+    children: ReactNode;
+    isOpen: boolean;
+    close: () => void;
+};
+
+const Modal: FC<Props> = ({ children, isOpen, close }) => {
+    return (
+        <Fragment>
+            {isOpen && (
+                <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center z-10">
+                    <div className="fixed top-0 left-0 h-screen w-screen bg-black opacity-50" onClick={close}></div>
+                    <div className="z-20 w-5/6 h-5/6 bg-white flex flex-col rounded-md">
+                        <div className="m-2">
+                            <CloseIcon onClick={close} className="cursor-pointer hover:bg-gray-100" fontSize="large" />
+                        </div>
+                        <div className="flex-1 overflow-hidden">{children}</div>
+                    </div>
+                </div>
+            )}
+        </Fragment>
+    );
+};
+
+export default Modal;
