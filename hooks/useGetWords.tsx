e33a84ae205b73f7489b0f22f355503adbad4069
浜田　俊祐 <hamashun0820@icloud.com>
2023-02-16 09:58:06 +0900

    getWordsとローディングの処理をhooks化

---
+++ hooks/useGetWords.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { Loading, Word } from '../types';
+
+const useGetWords = (isOpen: boolean, path: string, skipCondition?: boolean, time: number = 50) => {
+    const [words, setWords] = useState<Word[]>([]);
+    const [isLoading, setIsLoading] = useState<Loading>('hold');
+
+    const getWords = async () => {
+        const res = await fetch(path);
+        const words = (await res.json()) as Word[];
+        return words;
+    };
+
+    const init = async () => {
+        setIsLoading('loading');
+        const words = await getWords();
+        setWords(words);
+        await new Promise<void>((resolve) => {
+            setTimeout(() => resolve(), time);
+        });
+        setIsLoading('done');
+    };
+
+    useEffect(() => {
+        if (!skipCondition && isOpen) init();
+        return () => setIsLoading('hold');
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isOpen]);
+
+    return { words, isLoading };
+};
+
+export default useGetWords;
