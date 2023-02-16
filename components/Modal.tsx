94017c5823d6a3bad9559eddbfc83ca95e0ca758
浜田　俊祐 <hamashun0820@icloud.com>
2023-02-16 10:12:43 +0900

    モーダルの表示にフェードインをつけた

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,6 @@
-import React, { Dispatch, FC, Fragment, ReactNode, SetStateAction } from 'react';
+import React, { FC, ReactNode } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
+import Fade from '@mui/material/Fade';
 
 type Props = {
     children: ReactNode;
@@ -9,19 +10,25 @@ type Props = {
 
 const Modal: FC<Props> = ({ children, isOpen, close }) => {
     return (
-        <Fragment>
+        <>
             {isOpen && (
                 <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center z-10">
                     <div className="fixed top-0 left-0 h-screen w-screen bg-black opacity-50" onClick={close}></div>
-                    <div className="z-20 w-5/6 h-5/6 bg-white flex flex-col rounded-md">
-                        <div className="m-2">
-                            <CloseIcon onClick={close} className="cursor-pointer hover:bg-gray-100" fontSize="large" />
+                    <Fade in={isOpen}>
+                        <div className="z-20 w-5/6 h-5/6 bg-white flex flex-col rounded-md">
+                            <div className="m-2">
+                                <CloseIcon
+                                    onClick={close}
+                                    className="cursor-pointer hover:bg-gray-100"
+                                    fontSize="large"
+                                />
+                            </div>
+                            <div className="flex-1 overflow-hidden">{children}</div>
                         </div>
-                        <div className="flex-1 overflow-hidden">{children}</div>
-                    </div>
+                    </Fade>
                 </div>
             )}
-        </Fragment>
+        </>
     );
 };
 
