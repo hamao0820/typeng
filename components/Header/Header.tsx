09c696a6747b731bedcebb13e715ac7648c7b54e
浜田　俊祐 <hamashun0820@icloud.com>
2023-02-25 18:30:11 +0900

    headerのメッセージを変更

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/router';
 import BackButton from './BackButton';
 import SettingButton from '../Setting/SettingButton';
 import type { Mode } from '../../types';
+import { useAuthContext } from '../../Contexts/AuthProvider';
 
 type Props = {
     text: string;
@@ -18,10 +19,18 @@ type Props = {
 
 const Header: React.FC<Props> = ({ text, href, mode, collapseAll }) => {
     const router = useRouter();
+    const { user } = useAuthContext();
     return (
         <div className="w-full flex justify-between items-center">
             <BackButton href={href} text={text} />
-            <div>リストを右クリックすると単語一覧が表示されます</div>
+            <div className="flex items-center flex-col">
+                <div>リストを右クリックすると単語一覧が表示されます</div>
+                {user ? (
+                    <div>一覧から苦手な単語を登録することができます</div>
+                ) : (
+                    <div>ログインすると一覧から苦手な単語を登録することができます</div>
+                )}
+            </div>
             <div className="flex items-center">
                 <div
                     className="cursor-pointer border-2 border-solid border-blue-200 rounded-md mr-2"
