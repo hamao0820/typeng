f60adf0cb4f3341d3e5d7ba6fc19b14f2ab54704
浜田　俊祐 <hamashun0820@icloud.com>
2023-03-05 14:29:52 +0900

    練習モードでの日本語を大きくした 単語の表示をcomponent化した

---
+++ components/Worker/ShowWord.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useContext } from 'react';
+import { pronounce } from '../../utils';
+import { Word } from '../../types';
+import { pronounceVolumeContext } from '../../Contexts/PronounceProvider';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import FavoriteStar from '../Favorites/FavoriteStar';
+
+type Props = {
+    word: Word | null;
+    typed: string;
+    unTyped: string;
+};
+
+const ShowWord: FC<Props> = ({ word, typed, unTyped }) => {
+    const pronounceVolume = useContext(pronounceVolumeContext);
+    return (
+        <div className="w-screen h-full">
+            <div className="flex justify-between items-center w-32 ml-auto mr-20 my-6">
+                <div>{word && <FavoriteStar word={word} />}</div>
+                <div className="text-5xl whitespace-nowrap">id: {word && word.id}</div>
+            </div>
+            <div className="flex h-fit justify-start w-full ml-3 mt-16">
+                <div
+                    className="w-fit h-fit flex items-center justify-center bg-green-500 rounded-md"
+                    onClick={() => {
+                        if (word === null) return;
+                        pronounce(word.en, pronounceVolume);
+                    }}
+                >
+                    <VolumeUpIcon sx={{ height: '40vh', width: '40vh', minHeight: '200px', minWidth: '200px' }} />
+                </div>
+                <div className="flex flex-col justify-between mx-2 flex-1">
+                    <div className="line-clamp-2">
+                        <div className="text-8xl font-bold line-clamp-2 tracking-tighter" style={{ lineHeight: '100px' }}>
+                            {word && word.ja}
+                        </div>
+                    </div>
+                    <div className="whitespace-nowrap">
+                        <span className="text-7xl font-bold whitespace-nowrap">{typed.replaceAll(' ', '␣')}</span>
+                        <span className="text-7xl font-bold text-gray-300 whitespace-nowrap">
+                            {unTyped.replaceAll(' ', '␣')}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ShowWord;
--- pages/practice/[rank]/favorites.tsx
+++ pages/practice/[rank]/favorites.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, { FC, useCallback, useContext, useEffect, useRef } from 'react';
 import { FavoritesPageProps } from '../../../types/favorite';
 import { GetServerSideProps } from 'next';
 import { Mode, PathParams, Rank } from '../../../types';
@@ -9,12 +9,10 @@ import { soundEffectVolumeContext } from '../../../Contexts/SoundEffectProvider'
 import { typingVolumeContext } from '../../../Contexts/TypingVolumeProvider';
 import { pronounce, sound, typeSound } from '../../../utils';
 import Head from 'next/head';
-import FavoriteStar from '../../../components/Favorites/FavoriteStar';
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
-import Marquee from '../../../components/Worker/Marquee';
 import FavoriteHeader from '../../../components/Favorites/FavoriteHeader';
 import { useRouter } from 'next/router';
 import useHasFavorites from '../../../hooks/useHasFavorites';
+import ShowWord from '../../../components/Worker/ShowWord';
 
 export const getServerSideProps: GetServerSideProps<FavoritesPageProps> = async (context) => {
     const { rank } = context.params as PathParams;
@@ -27,13 +25,11 @@ const Favorites: FC<FavoritesPageProps> = ({ rankWords }) => {
     const pronounceVolume = useContext(pronounceVolumeContext);
     const soundEffectVolume = useContext(soundEffectVolumeContext);
     const typingVolume = useContext(typingVolumeContext);
-    const [isOver, setIsOver] = useState<boolean>(false);
     const ref = useRef<HTMLDivElement>(null);
-    const contentRef = useRef<HTMLSpanElement>(null);
-
     const router = useRouter();
     const { rank } = router.query as { rank: Rank };
     const mode = router.pathname.split('/')[1] as Mode; // /mode/[rank]/favorites
+    
     if (!useHasFavorites(rank)) {
         router.push({ pathname: `/${mode}` });
     }
@@ -43,13 +39,6 @@ const Favorites: FC<FavoritesPageProps> = ({ rankWords }) => {
             return;
         }
         pronounce(word.en, pronounceVolume / 100);
-        const content = contentRef.current;
-        if (content === null) return;
-        if (800 <= content.clientWidth) {
-            setIsOver(true);
-        } else {
-            setIsOver(false);
-        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [word]);
 
@@ -73,6 +62,7 @@ const Favorites: FC<FavoritesPageProps> = ({ rankWords }) => {
         },
         [unTyped, typingVolume, soundEffectVolume]
     );
+
     const handleKeyDown = useCallback(
         (e: React.KeyboardEvent<HTMLDivElement> | KeyboardEvent) => {
             handleWord(e);
@@ -85,55 +75,17 @@ const Favorites: FC<FavoritesPageProps> = ({ rankWords }) => {
         window.addEventListener('keydown', handleKeyDown);
         return () => window.removeEventListener('keydown', handleKeyDown);
     }, [handleKeyDown]);
+
     return (
         <div className="h-screen w-screen overflow-hidden" ref={ref}>
             <Head>
                 <title>practice</title>
             </Head>
-            <FavoriteHeader text="選択画面に戻る" href="/practice" mode="practice" />
-            <div className="h-4/5 relative w-full">
-                {word && (
-                    <div className="absolute top-5 right-12 flex justify-between items-center w-32">
-                        <div>
-                            <FavoriteStar word={word} />
-                        </div>
-                        <div className="text-3xl whitespace-nowrap">id: {word.id}</div>
-                    </div>
-                )}
-                <div className="flex h-fit justify-start absolute top-1/3 left-60 w-full">
-                    <div
-                        className="w-fit h-fit flex items-center justify-center p-2 bg-green-500 rounded-md"
-                        onClick={() => {
-                            if (word === null) return;
-                            pronounce(word.en, pronounceVolume);
-                        }}
-                    >
-                        <VolumeUpIcon style={{ width: '13rem', height: '13rem' }} />
-                    </div>
-                    <div className="flex flex-col justify-between ml-5" style={{ width: '800px' }}>
-                        <div className={isOver ? 'hidden' : ''}>
-                            <span className="text-7xl font-bold whitespace-nowrap h-fit max-w-4xl overflow-hidden text-ellipsis inline-block">
-                                {word?.ja}
-                            </span>
-                        </div>
-
-                        {word !== null && isOver && <Marquee content={word.ja} />}
-                        <div className="whitespace-nowrap">
-                            <span className="text-8xl font-bold whitespace-nowrap">{typed.replaceAll(' ', '␣')}</span>
-                            <span className="text-8xl font-bold text-gray-300 whitespace-nowrap">
-                                {unTyped.replaceAll(' ', '␣')}
-                            </span>
-                        </div>
-                    </div>
+            <div className="flex flex-col">
+                <FavoriteHeader text="選択画面に戻る" href="/practice" mode="practice" />
+                <div className="flex-1">
+                    <ShowWord {...{ word, typed, unTyped }} />
                 </div>
-            </div>
-            <div className="invisible">
-                <span
-                    className="text-7xl font-bold whitespace-nowrap h-fit max-w-4xl overflow-hidden text-ellipsis inline-block"
-                    ref={contentRef}
-                >
-                    {word?.ja}
-                </span>
             </div>
         </div>
     );
