03c495be28c60320114181e6b4cbe386a6eddf57
浜田　俊祐 <hamashun0820@icloud.com>
2023-03-06 17:46:46 +0900

    アイコンを削除

--- components/Worker/ShowWord.tsx
+++ components/Worker/ShowWord.tsx
@@ -1,10 +1,7 @@
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
-import React, { FC, useContext, useMemo } from 'react';
+import React, { FC, useMemo } from 'react';
 
-import { pronounceVolumeContext } from '../../Contexts/PronounceProvider';
 import { useTextSizeContext } from '../../Contexts/TextSizeProvider';
 import { Word } from '../../types';
-import { pronounce } from '../../utils';
 import FavoriteStar from '../Favorites/FavoriteStar';
 
 type Props = {
@@ -17,7 +14,6 @@ type Props = {
 };
 
 const ShowWord: FC<Props> = ({ word, typed, unTyped, showUnTyped = true, showHint = false, progress }) => {
-    const pronounceVolume = useContext(pronounceVolumeContext);
     const { textSize } = useTextSizeContext();
     const textSizeClassName = useMemo(() => {
         switch (textSize) {
@@ -32,34 +28,25 @@ const ShowWord: FC<Props> = ({ word, typed, unTyped, showUnTyped = true, showHin
         }
     }, [textSize]);
     return (
-        <div className="w-screen h-full">
-            <div className="flex justify-end w-screen my-6">
+        <div className="w-screen">
+            <div className="flex justify-end w-full my-6">
                 {progress && <div className="text-5xl whitespace-nowrap mx-10">{progress}</div>}
                 <div className="flex justify-between items-center w-32 mr-28">
                     <div>{word && !progress && <FavoriteStar word={word} />}</div>
                     <div className="text-5xl whitespace-nowrap">id: {word && word.id}</div>
                 </div>
             </div>
-            <div className="flex h-fit justify-start w-full ml-3 mt-16">
-                <div
-                    className="w-fit h-fit flex items-center justify-center bg-green-500 rounded-md"
-                    onClick={() => {
-                        if (word === null) return;
-                        pronounce(word.en, pronounceVolume);
-                    }}
-                >
-                    <VolumeUpIcon sx={{ height: '40vh', width: '40vh', minHeight: '200px', minWidth: '200px' }} />
-                </div>
-                <div className="flex flex-col justify-between mx-2 flex-1">
-                    <div className="line-clamp-2">
+            <div className="flex justify-center w-full">
+                <div className="flex flex-col justify-between items-center w-full">
+                    <div className="h-52 mx-1">
                         <div
-                            className={`${textSizeClassName} font-bold line-clamp-2 tracking-tighter`}
+                            className={`${textSizeClassName} font-bold line-clamp-2 tracking-tighter text-center`}
                             style={{ lineHeight: '100px' }}
                         >
                             {word && word.ja}
                         </div>
                     </div>
-                    <div className="whitespace-nowrap">
+                    <div className="whitespace-nowrap flex items-center justify-center w-5/6 h-20 border-b-4 border-collapse border-gray-300 my-2">
                         <span className="text-7xl font-bold whitespace-nowrap">{typed.replaceAll(' ', '␣')}</span>
                         {showUnTyped ? (
                             <span className="text-7xl font-bold text-gray-300 whitespace-nowrap">
