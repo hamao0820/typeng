1824ca964fd0228a31dd83e007829d3aa16a1464
浜田　俊祐 <hamashun0820@icloud.com>
2023-03-08 12:58:47 +0900

    ogpと説明文の実装

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,18 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="ja" prefix="og: http://ogp.me/ns#">
             <Head>
                 <link rel="icon" href="/favicons/favicon.ico" />
+                <meta name="author" content="hamao" />
+                <meta
+                    name="description"
+                    content="英単語をタイピングで練習するアプリです。4つのモードで英単語の暗記とタイピングの練習を同時に行うことができます。単語はTOEFL用のものです"
+                ></meta>
+                <meta
+                    name="og:description"
+                    content="英単語をタイピングで練習するアプリです。4つのモードで英単語の暗記とタイピングの練習を同時に行うことができます。単語はTOEFL用のものです"
+                />
             </Head>
             <body>
                 <Main />
